06b4056b8667bb20aaae6a133c3d77fd48cc2a0b
anish2210 <anishjaiswal1220@gmail.com>
2024-06-25 10:24:59 +0530

    Ai Object Detector

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
         Object Detector
       </h1>
-      
+
       <ObjectDetection />
 
       <h2 className="gradient-title p-8 text-3xl md:text-4xl lg:text-5xl tracking-tighter md:px-6">
-        Objects it can detect - <a href="http://google.com">Click to know</a>
+        Objects it can detect - <a href="https://github.com/anish2210/Object-Detector?tab=readme-ov-file#display-names">Click to know</a>
       </h2>
     </main>
   );
